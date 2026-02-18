b086d5427966cb3d37b81c5db8968300bea86d10
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-18 19:05:11 +0530

    commit

--- src/Number/TrailingZeroes.java
+++ src/Number/TrailingZeroes.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class TrailingZeroes {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-
         int n= scanner.nextInt();
-
         int count=0;
 
 /* Method 1: Here we traverse from 1 to n
@@ -18,13 +16,11 @@ public class TrailingZeroes {
         }
 
  */
-
  /* Method 2: In this method reducing the value of n by dividing the n by 5 */
         while(n>0){
             n/=5;
             count+=n;
         }
-
         System.out.println(count);
     }
 }
