719b300dfb21326410bf24b874de694fb2c8bea1
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-26 16:11:53 +0530

    commit

--- src/TCS_Tag/ClimbingStairs.java
+++ src/TCS_Tag/ClimbingStairs.java
@@ -1,7 +1,28 @@
 package TCS_Tag;
 
+import java.util.Scanner;
+
 public class ClimbingStairs {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+
+        if(n<=2){
+            System.out.println(n);
+        }
+
+        int prev1=2;
+        int prev2=1;
+
+        for(int i=3;i<n;i++){
+            int curr=prev1+prev2;
+
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        System.out.println(prev1);
+
 
     }
 }
