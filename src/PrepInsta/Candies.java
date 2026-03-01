b143bd08a295707d08a7b744e6108b1d814d298a
parasbramhankar <parasbramhankar.in@gmail.com>
2026-03-01 19:35:45 +0530

    commit

--- src/PrepInsta/Candies.java
+++ src/PrepInsta/Candies.java
@@ -1,6 +1,10 @@
 package PrepInsta;
 /**
- * There is a JAR full of candies for sale at a mall counter. JAR has the capacity N, that is JAR can contain maximum N candies when JAR is full. At any point of time. JAR can have M number of Candies where M<=N. Candies are served to the customers. JAR is never remain empty as when last k candies are left. JAR if refilled with new candies in such a way that JAR get full.
+ * There is a JAR full of candies for sale at a mall counter. JAR has the capacity N,
+ * that is JAR can contain maximum N candies when JAR is full.
+ * At any point of time. JAR can have M number of Candies where M<=N.
+ * Candies are served to the customers. JAR is never remain empty as when last k candies are left.
+ * JAR if refilled with new candies in such a way that JAR get full.
  * Write a code to implement above scenario. Display JAR at counter with available number of candies. Input should be the number of candies one customer can order at point of time. Update the JAR after each purchase and display JAR at Counter.
  *
  * Output should give number of Candies sold and updated number of Candies in JAR.
